e137bdd879b4fc3cc474b28eb8d8155eabad087b
Luis Felipe Duarte Herrera <LuisFDuarte@users.noreply.github.com>
2023-01-16 11:36:22 -0500

    Add files via upload
    Version inicial microservicio estaciones fiware

--- main.py
+++ main.py
@@ -5,48 +5,28 @@ import os
 from api import app as app_fastapi
 from scheduler import app as app_rocketry
 
-# Load the environment variables from the keys.env file
-# load_dotenv("keys.env")
-
-# Get the port from the environment variable
+#load_dotenv("keys.env")
 PORT = int(os.getenv('PORT'))
 print(PORT)
-
 class Server(uvicorn.Server):
-    """
-    Customized uvicorn.Server
+    """Customized uvicorn.Server
     Uvicorn server overrides signals and we need to include
-    Rocketry to the signals.
-    """
+    Rocketry to the signals."""
     def handle_exit(self, sig: int, frame) -> None:
-        """
-        Handle the exit signal by shutting down the session
-        before calling the parent's handle_exit method.
-        """
         app_rocketry.session.shut_down()
         return super().handle_exit(sig, frame)
 
-
 async def main():
-    """
-    Run Rocketry and FastAPI
-    """
-    # Create an instance of the Server class
+    "Run Rocketry and FastAPI"
     server = Server(config=uvicorn.Config(app_fastapi, workers=1, loop="asyncio", host="0.0.0.0", port=PORT))
     print(app_fastapi)
-
-    # Run the server.serve() and app_rocketry.serve() as async tasks
     api = asyncio.create_task(server.serve())
     sched = asyncio.create_task(app_rocketry.serve())
-
-    # Wait for both tasks to complete
     await asyncio.wait([sched, api])
-
 
 if __name__ == "__main__":
     # Print Rocketry's logs to terminal
     logger = logging.getLogger("rocketry.task")
     logger.addHandler(logging.StreamHandler())
-
     # Run both applications
     asyncio.run(main())
--- scheduler.py
+++ scheduler.py
@@ -9,28 +9,22 @@ import os
 import time
 import json
 
-# Loading environment variables from keys.env file
 load_dotenv("keys.env")
-
-# Setting the environment variables
 APP_KEY = str(os.getenv("APP_KEY"))
 API_KEY = str(os.getenv("API_KEY"))
 SERVER = str(os.getenv("SERVER")) # 200.3.144.214 10.61.3.135
 ID = str(os.getenv("ID"))
-
 # Creating the Rocketry app
 app = Rocketry(config={"task_execution": "async"})
 
 def get_meteorological_data():
-    """
-    Retrieve the meteorological data from the API.
-    """
-    url = "https://api.ambientweather.net/v1/devices?apiKey={}&applicationKey={}".format(API_KEY, APP_KEY)
-
+    url = "https://api.ambientweather.net/v1/devices?apiKey=%s&applicationKey=%s" % (
+        API_KEY,
+        APP_KEY,
+    )
     try:
-        # Send a GET request
         response = requests.request("GET", url, timeout=3)
-        # Raise exception for non-200 status codes
+        # If the response was successful, no Exception will be raised
         response.raise_for_status()
     except HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
@@ -39,30 +33,20 @@ def get_meteorological_data():
     except Exception as err:
         print(f"Other error occurred: {err}")
     else:
-        # If the request is successful
         data = response.json()
-        data_processed = convert_units(data[0]['lastData'])
+        data_processed= convert_units(data[0]['lastData'])
         joblib.dump(data_processed, "data/data")
         joblib.dump(data[0]['lastData'], "data/raw_data")
 
 def convert_units(data):
-    """Convert the unit of the provided data and return the processed data.
-
-    Args:
-    data (dict): A dictionary of key-value pairs where the key is the name of the data and the value is the data's value.
-
-    Returns:
-    dict: A dictionary of key-value pairs where the key is the name of the processed data and the value is the processed data's value.
-
-    """
     processed_data={}
     for key, value in data.items():
         if key == "tempf":
             processed_data["Temperature"] = (value - 32) * (5/9) # F to C
         elif key == "humidity":
-            processed_data["Humidity"] = value
+            processed_data["Humidity"] = (value)
         elif key == "humidityin":
-            processed_data["Humidity_inside"] = value
+            processed_data["Humidity_inside"] = (value)
         elif key == "tempinf":
             processed_data["Temperature_inside"] = (value - 32) * (5/9) # F to C
         elif key == "windspeedmph":
@@ -95,30 +79,29 @@ def convert_units(data):
             processed_data["CO2"] = value
     return processed_data
 
+# Creating some tasks
 def process_data():
-    """Load data from the file and pass it to the request_to_fiware function."""
     processed_data = {}
     processed_data = joblib.load("data/data")
     request_to_fiware(processed_data)
 
-
 def request_to_fiware(data):
-    """Make a POST request to FIWARE server with the provided data and headers.
-
-    Handle any errors that may occur during the request.
-    """
     url = "http://"+SERVER+":1026/v2/entities/"+ID+"/attrs"
 
-    payload = {}
-    for key, value in data.items():
+    payload = {
+        # "id": "WS_HOMESOUL_ECOVILLA",
+        # "type": "WS",
+    }
+    for key,value in data.items():
         payload[key] = {
             "type": "Number",
             "value": value,
         }
     headers = {'Content-Type': 'application/json'}
-    print("url", url)
-    print("headers", headers)
+    print("url",url)
+    print("headers",headers)
     print(json.dumps(payload, indent=4))
+
 
     try:
         response = requests.post(url, json=payload, headers=headers, timeout=3)
@@ -131,21 +114,75 @@ def request_to_fiware(data):
     except Exception as err:
         print(f"Other error occurred: {err}")
     else:
-        data = response  # .json()
+        data = response#.json()
         print(data)
     time.sleep(0.3)
 
-
-@app.task("every 45 seconds")
+@app.task("every 1 minute")
 async def get_weather_station_data():
-    """Retrieve meteorological data."""
     get_meteorological_data()
 
 @app.task("every 1 minute")
 async def send_data_to_fiware():
-    """Process the data and send it to the specified server."""
     process_data()
 
 if __name__ == "__main__":
     # If this script is run, only Rocketry is run
     app.run()
+
+#%% funciones para suscripcion inicial de los datos
+# def process_data2():
+#     processed_data = {}
+#     processed_data = joblib.load("data/data")
+#     request_to_fiware2(processed_data)
+
+# @app.task("every 1 minute")
+# async def suscription_fiware():
+#     process_data2()
+# def request_to_fiware2(data):
+#     url = "http://"+SERVER+":1026/v2/subscriptions"
+
+#     payload = {
+#         "description": "Suscripcion a cambios de contexto Estacion meteorologica HomeSoul Ecovilla 1",
+#         "subject": {
+#             "entities": [{
+#                 "idPattern": ".*",
+#                 "type": "WS",
+#             }],
+#             "condition": {
+#                 "attrs": []
+#             }
+#         },
+#         "notification": {
+#             "attrs": [
+#                 "id",
+#             ],
+#             "http": {
+#                 "url": "http://quantumleap:8668/v2/notify"
+#             },
+#             "metadata": [
+#                 "dateCreated",
+#                 "dateModified"
+#             ]
+#         }
+#     }
+#     for key,value in data.items():
+#         payload["subject"]["condition"]["attrs"].append(key)
+#         payload["notification"]["attrs"].append(key)
+
+#     headers = {'Content-Type': 'application/json'}
+#     print(json.dumps(payload, indent=4))
+#     try:
+#         response = requests.post(url, json=payload, headers=headers, timeout=3)
+#         # If the response was successful, no Exception will be raised
+#         response.raise_for_status()
+#     except HTTPError as http_err:
+#         print(f"HTTP error occurred: {http_err}")
+#     except Timeout as err:
+#         print(f"The request timed out: {err}")
+#     except Exception as err:
+#         print(f"Other error occurred: {err}")
+#     else:
+#         data = response#.json()
+#         print(data)
+#     time.sleep(0.5)
